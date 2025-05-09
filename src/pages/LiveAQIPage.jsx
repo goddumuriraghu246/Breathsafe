f2968fb27e6233773c3b1125577e91bc3bd72fc8
raghu <gr3129223@gmail.com>
2025-05-09 18:24:43 +0530

    live aqi heading fix

--- src/pages/LiveAQIPage.jsx
+++ src/pages/LiveAQIPage.jsx
@@ -262,7 +262,7 @@ const LiveAQIPage = () => {
   const advisory = getAQIAdvisory(aqiData?.value);
 
   return (
-    <div className="pb-16 pt-25">
+    <div className="pt-20 pb-16">
       <div className="container-custom">
         <motion.div
           className="mb-8 text-center"
